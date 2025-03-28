defdc3ae9d3ac459b4a447ef1db07868f9f51a58
FallingRain <FallingRain727@outlook.com>
2025-03-28 15:51:12 +0800

    增加两个前端界面

--- vue-front/src/main.ts
+++ vue-front/src/main.ts
@@ -13,8 +13,12 @@ import App from './App.vue'
 // Composables
 import { createApp } from 'vue'
 
+import ElementPlus from 'element-plus'; // 引入 Element Plus
+import 'element-plus/dist/index.css'; // 引入 Element Plus 的样式
+
 const app = createApp(App)
 
 registerPlugins(app)
 
+app.use(ElementPlus); // 全局注册 Element Plus
 app.mount('#app')
